d476908610870d75ebbf94c452218e9eb9b1cf40
pbenton6 <patrick.benton6396@gmail.com>
2021-02-08 16:01:38 -0600

    commit tree dd663a217b0f11787fe2711f4500ca2c64939e17

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,31 @@
+class Doctor
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |app|
+            app.doctor == self
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments.map do |app|
+            app.patient
+        end
+    end
+
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |app|
+            app.patient == self
+        end
+    end
+
+    def doctors
+        appointments.map do |app|
+            app.doctor
+        end
+    end
+end
